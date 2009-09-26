33c627dffd3f608c8c4132e3d086ca0f71e1d710
Thomas Rawyler <rawyler@gmail.com>
2009-09-26 12:38:42 +0200

    parsing with new lines now

--- src/main/scala/ch/puzzle/todoapp/parsers/TaskpaperParser.scala
+++ src/main/scala/ch/puzzle/todoapp/parsers/TaskpaperParser.scala
@@ -3,11 +3,13 @@ package ch.puzzle.todoapp.parsers
 import scala.util.parsing.combinator._
 
 class TaskpaperParser extends JavaTokenParsers {
-
-  /**
-   * Name for project and task
-   */
-  private def name : Parser[String] = """[^:\n\r\@]*""".r
+  override def skipWhitespace = false
+  
+  private def space = "[ \\n]+".r
+  
+  def root : Parser[Any] = "todo"~opt(" ")~"{"~space~rep(member)~"}"
+  
+  def member : Parser[Any] = (project | task | note)~space
   
   /**
    * A task is a line that begins with a hyphen followed by a space ('- ')
@@ -20,7 +22,12 @@ class TaskpaperParser extends JavaTokenParsers {
    * A project is a line that isn't a task and ends with a colon (':')
    * or a colon (':\n') followed by a newline.
    */
-  def project : Parser[Any] = not(task)~name~":"~opt("\n")
+  def project : Parser[Any] = not(task)~name~":"
+  
+  /**
+  * A note is any line that doesn't match the task or project rules.
+  */
+  def note : Parser[Any] = not(task)~not(project)~not(tag)~noteText
   
   /**
    * A context tag has the form "@tag", i.e. it starts with an "at" character
@@ -31,14 +38,17 @@ class TaskpaperParser extends JavaTokenParsers {
    */
   def tag : Parser[Any] = tagName~opt(value)
   
-  private def tagName : Parser[String] = """\@[\w]*""".r
-  
+  /**
+   * The value of a tag
+   */
   def value : Parser[Any] = name
   
   /**
-   * A note is any line that doesn't match the task or project rules.
-   */
-  def note : Parser[Any] = not(task)~not(project)~not(tag)~noteText
+  * Name for project and task
+  */
+  private def name : Parser[String] = """[^:\n\r\@]*""".r
+  
+  private def tagName : Parser[String] = """\@[\w]*""".r
     
   private def noteText : Parser[String] = """[^\n\r]*""".r
 }
--- src/test/scala/ch/puzzle/todoapp/parsers/TaskpaperParserTest.scala
+++ src/test/scala/ch/puzzle/todoapp/parsers/TaskpaperParserTest.scala
@@ -2,50 +2,55 @@ package ch.puzzle.todoapp.parsers
 
 import junit.framework.TestSuite
 import _root_.org.junit.Test
+import _root_.org.junit.Before
 import scala.util.parsing.combinator._
 
 class TaskpaperParserTest extends TestSuite {
+  private val parser = new TaskpaperParser
   
   @Test def testSimpleTask() {
     val input = "- this is a task without tags."
-    val parser = new TaskpaperParser
     assert(parser.parseAll(parser.task, input).successful)
   }
   
   @Test def testWrongTask() {
     val input = "this is not a task."
-    val parser = new TaskpaperParser
     assert(!parser.parseAll(parser.task, input).successful)
   }
   
   @Test def testSimpleTag() {
     val input = "@tag"
-    val parser = new TaskpaperParser
     assert(parser.parseAll(parser.tag, input).successful)
   }
   
   @Test def testTagWithValue() {
     val input = "@tag(i am the value)"
-    val parser = new TaskpaperParser
     assert(parser.parseAll(parser.tag, input).successful)
   }
   
   @Test def testNote() {
     val input = "this is a simple note"
-    val parser = new TaskpaperParser
     assert(parser.parseAll(parser.note, input).successful)
   }
   
   @Test def testNoNote() {
     val input = "- this is a task and not a note"
-    val parser = new TaskpaperParser
     assert(!parser.parseAll(parser.note, input).successful)
   }
   
   @Test def testProject() {
     val input = "this is a project:"
-    val parser = new TaskpaperParser
     assert(parser.parseAll(parser.project, input).successful)
+  }
+  
+  @Test def testRootWithTaskAndProject() {
+    val input = """todo {
+                       |the project:
+                       |- a task
+                       |another project:
+                       |	- another project's task
+                       |}""".stripMargin
+    assert(parser.parseAll(parser.root, input).successful)
   }
   
 }
