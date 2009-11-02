e65f2d240cf368a24e89c0fff542280b9690668a
Thomas Rawyler <rawyler@gmail.com>
2009-11-02 15:39:01 +0100

    introduced production combinator

--- src/main/scala/ch/puzzle/todoapp/parsers/TaskpaperParser.scala
+++ src/main/scala/ch/puzzle/todoapp/parsers/TaskpaperParser.scala
@@ -2,32 +2,49 @@ package ch.puzzle.todoapp.parsers
 
 import scala.util.parsing.combinator._
 
-class TaskpaperParser extends JavaTokenParsers {
+class TaskpaperParser extends RegexParsers {
   override def skipWhitespace = false
   
   private def space = "[ \\n]+".r
   
-  def root : Parser[Any] = "todo"~opt(" ")~"{"~space~rep(member)~"}"
+  private def indent = "\t".r
   
-  def member : Parser[Any] = (project | task | note)~space
+  def root : Parser[TaskList] = ( "todo {"~space~repsep(member, space)~"}" ) ^^
+    { case start~space~members~end => TaskList(members) }
+  
+  def member : Parser[Member] = (project | task | note)
+  
+  def child : Parser[Member] = indent~member ^^ { case indent~member => member }
   
   /**
    * A task is a line that begins with a hyphen followed by a space ('- ')
    * which can optionally be prefixed (i.e indented) with tabs or spaces.
    * A task can have zero or more context tags.
    */
-  def task : Parser[Any] = "- "~name~rep(tag)
+  def task : Parser[Task] =
+    ( "- "~name~repsep(tag, " ")~space~rep(child) ) ^^
+      { case "- "~name~tags~space~members => Task(name.toString, tags, members) } |
+    ( "- "~name~repsep(tag, " ") ) ^^
+      { case "- "~name~tags => Task(name.toString, tags, Nil) }
 
   /**
    * A project is a line that isn't a task and ends with a colon (':')
    * or a colon (':\n') followed by a newline.
    */
-  def project : Parser[Any] = not(task)~name~":"
+  def project : Parser[Project] =
+    ( not(task)~name~":"~space~rep(child) ) ^^
+      { case t~name~colon~space~members => Project(name.toString, members) } |
+    ( not(task)~name~":" ) ^^
+      { case t~name~colon => Project(name.toString, Nil) }
   
   /**
   * A note is any line that doesn't match the task or project rules.
   */
-  def note : Parser[Any] = not(task)~not(project)~not(tag)~noteText
+  def note : Parser[Note] =
+    ( not(task)~not(project)~not(tag)~noteText~space~rep(child) ) ^^
+      { case task~project~tag~name~space~members => Note(name.toString, members) } |
+    ( not(task)~not(project)~not(tag)~noteText ) ^^
+      { case task~project~tag~name => Note(name.toString, Nil) }
   
   /**
    * A context tag has the form "@tag", i.e. it starts with an "at" character
@@ -36,19 +53,27 @@ class TaskpaperParser extends JavaTokenParsers {
    * follows the tag word (no whitespace between) and is enclosed by
    * '(' and ')'. The value text inside can have whitespace, but no newlines.
    */
-  def tag : Parser[Any] = tagName~opt(value)
+  def tag : Parser[Tag] = 
+    ( tagIdentifier~tagName~"("~"""[^:\n\r\@\(\)]*""".r~")" ) ^^
+      { case at~tagName~o~value~c => Tag(tagName.toString, value.toString) } |
+    ( tagIdentifier~tagName ) ^^
+      { case at~tagName => new Tag(tagName.toString) }
   
-  /**
-   * The value of a tag
-   */
-  def value : Parser[Any] = name
+  private def name = """[^:\n\r\@]*""".r
   
-  /**
-  * Name for project and task
-  */
-  private def name : Parser[String] = """[^:\n\r\@]*""".r
+  private def tagName = """[\w]*""".r
   
-  private def tagName : Parser[String] = """\@[\w]*""".r
+  private def tagIdentifier = "@".r
     
-  private def noteText : Parser[String] = """[^\n\r]*""".r
+  private def noteText = """[^\n\r]*""".r
 }
+
+abstract trait Member {
+  val name: String
+  val members: List[Member]
+}
+case class Tag(name: String, value: String) { def this(name: String) = this(name, null) }
+case class Task(name: String, tags: List[Tag], members: List[Member]) extends Member
+case class Note(name: String, members: List[Member]) extends Member
+case class Project(name: String, members: List[Member]) extends Member
+case class TaskList(members: List[Member])
--- src/test/scala/ch/puzzle/todoapp/parsers/TaskpaperParserTest.scala
+++ src/test/scala/ch/puzzle/todoapp/parsers/TaskpaperParserTest.scala
@@ -4,46 +4,56 @@ import junit.framework.TestSuite
 import _root_.org.junit.Test
 import _root_.org.junit.Before
 import scala.util.parsing.combinator._
+import org.junit._
+import Assert._
 
 class TaskpaperParserTest extends TestSuite {
   private val parser = new TaskpaperParser
   
-  @Test def testSimpleTask() {
+  @Test def shouldParseSimpleTask() {
     val input = "- this is a task without tags."
     assert(parser.parseAll(parser.task, input).successful)
   }
   
-  @Test def testWrongTask() {
+  @Test def shouldParseTaskWithTags() {
+    val input = "- task @tag1 @tag2 @tag3(this should work as well) @tag4"
+    assert(parser.parseAll(parser.task, input).successful)
+  }
+  
+  @Test def shouldParseWrongTask() {
     val input = "this is not a task."
     assert(!parser.parseAll(parser.task, input).successful)
   }
   
-  @Test def testSimpleTag() {
-    val input = "@tag"
+  @Test def shouldParseSimpleTag() {
+    val input = "@nameOfTheTag"
     assert(parser.parseAll(parser.tag, input).successful)
+    assertEquals(Tag("nameOfTheTag", null), parser.parseAll(parser.tag, input).get)
   }
   
-  @Test def testTagWithValue() {
+  @Test def shouldParseTagWithValue() {
     val input = "@tag(i am the value)"
     assert(parser.parseAll(parser.tag, input).successful)
+    assertEquals(Tag("tag", "i am the value"), parser.parseAll(parser.tag, input).get)
   }
   
-  @Test def testNote() {
+  @Test def shouldParseNote() {
     val input = "this is a simple note"
     assert(parser.parseAll(parser.note, input).successful)
+    assertEquals(Note("this is a simple note", List()), parser.parseAll(parser.note, input).get)
   }
   
-  @Test def testNoNote() {
+  @Test def shouldParseNoNote() {
     val input = "- this is a task and not a note"
     assert(!parser.parseAll(parser.note, input).successful)
   }
   
-  @Test def testProject() {
+  @Test def shouldParseProject() {
     val input = "this is a project:"
     assert(parser.parseAll(parser.project, input).successful)
   }
   
-  @Test def testRootWithTaskAndProject() {
+  @Test def shouldParseRootWithTaskAndProject() {
     val input = """todo {
                        |the project:
                        |- a task
@@ -51,6 +61,38 @@ class TaskpaperParserTest extends TestSuite {
                        |	- another project's task
                        |}""".stripMargin
     assert(parser.parseAll(parser.root, input).successful)
+  }
+  
+  @Test def shouldParseTaskWithChildTask() {
+	val input = """- parent task
+		  |	- child task""".stripMargin
+	// println(parser.parseAll(parser.member, input))
+    assert(parser.parseAll(parser.member, input).successful)
+  }
+  
+  @Test def shouldParseChildren() {
+    val input = """todo{
+		  |- parent task
+		  |	- first child
+		  |	a child note
+          | - second child
+          | 	- child of second
+          |project:
+          |	- task
+		  |}""".stripMargin
+    // println(parser.parseAll(parser.root, input))
+    assert(parser.parseAll(parser.root, input).successful)
+  }
+  
+  @Test def shouldParseChild() {
+    val input = "	child note"
+    println(parser.parseAll(parser.child, input))
+    assert(parser.parseAll(parser.child, input).successful)
+  }
+  
+  @Test def shouldNotParseFalseChild() {
+    val input = "not a child note"
+    assert(!parser.parseAll(parser.child, input).successful)
   }
   
 }
