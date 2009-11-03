293a6a3026354c4a306f79145c07695974c4a491
Thomas Rawyler <rawyler@gmail.com>
2009-11-03 16:20:01 +0100

    simplified syntax

--- src/main/scala/ch/puzzle/todoapp/parsers/TaskpaperParser.scala
+++ src/main/scala/ch/puzzle/todoapp/parsers/TaskpaperParser.scala
@@ -77,9 +77,11 @@ abstract trait Member {
   val name: String
   val members: List[Member]
 }
-case class Tag(name: String, value: String) { def this(name: String) = this(name, null) }
+case class Tag(name: String, value: String) {
+  def this(name: String) = this(name, null)
+}
 
-case class Task(name: String, tags: List[Tag], members: List[Member]) extends Member
+case class Task(name: String, tags: List[Tag], members: List[Member]) extends Member 
 
 case class Note(name: String, members: List[Member]) extends Member
 
